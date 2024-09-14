0e33a351db7479479bd957eca0740e860dbe6732
VadymPriadun <pryadun.vv06@gmail.com>
2024-09-14 20:22:42 +0200

    Add targets, livraries and executable project

---
+++ Ep007/source/include/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#include <string>
+#include <iostream>
+class Dog
+{
+private:
+    std::string dog_name{ "Puffy" };
+public:
+    explicit Dog(std::string name_param);
+    Dog() = default;
+    ~Dog();
+    
+    std::string get_name() const{
+        return dog_name;
+    }
+    
+    void set_dog_name(const std::string & name){
+        dog_name = name;
+    }
+	
+	void print_info(){
+		std::cout << "Dog [ name : " << dog_name << " ]" <<  std::endl;
+	}
+    
+
+};
+
+
+#endif // DOG_H
